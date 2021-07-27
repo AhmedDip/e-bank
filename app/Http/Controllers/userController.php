c398b84febaa569a20d0fec26d454a5e380c5c98
Ahmed Rasidun Bari Dip <67531074+AhmedDip@users.noreply.github.com>
2021-07-27 22:14:45 +0600

    Update userController.php

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -11,25 +11,10 @@ class userController extends Controller
         return view('user.userList');
     }
 
-    public function addUser()
-    {
-
-        return view('user.addUser');
-    }
-
-    public function blockUser()
-    {
-        return view('user.blockUser');
-    }
 
     public function changePass()
     {
         return view('user.changePassword');
-    }
-
-    public function clientReq()
-    {
-        return view('user.clientReq');
     }
 
     public function deleteUser()
@@ -47,33 +32,18 @@ class userController extends Controller
         return view('user.changePassword');
     }
 
-    public function editUser()
-    {
-        return view('user.editUser');
-    }
+
 
     public function pendingUser()
     {
         return view('user.pendingUser');
     }
 
-    public function postNotices()
-    {
-        return view('user.postNotices');
-    }
 
     public function profile()
     {
         return view('user.profile');
     }
 
-    public function unblockUser()
-    {
-        return view('user.unblockUser');
-    }
-
-    public function userServices()
-    {
-        return view('user.userServices');
-    }
+ 
 }
